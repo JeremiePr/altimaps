b9015028a959a7ed15134251ed8f6b3b838f67e0
Jeremie Primas <jeremie.primas@gmail.com>
2018-06-08 16:40:02 +0200

    commented code

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
-const pattern = "";
+/**
+ * background.js
+ * created on 06/08/2018
+ * by Jeremie Primas
+ */
 
+
+// Store the last request URL called
 let url = "";
 
+// Create a listener that intercepts HTTP request calls with URLs that match the following pattern
 chrome.webRequest.onBeforeRequest.addListener(req => {
+	// Prevent from calling again the same request
 	if(req.url !== url) {
 		url = req.url;
+		// Calls the server with the request made by the page
 		$.ajax({
 			url: url,
 			data: null,
@@ -20,7 +29,11 @@ chrome.webRequest.onBeforeRequest.addListener(req => {
 	urls: ["https://www.google.com/maps/photometa/v1*"]
 });
 
-let onRequestSuccess = res => {
+/**
+ * Function called on request success
+ * @param res 
+ */
+function onRequestSuccess(res) {
 	let altitude = getAltitude(res);
 	chrome.tabs.query({
 		active: true
@@ -32,11 +45,21 @@ let onRequestSuccess = res => {
 	});
 }
 
-let onRequestError = err => {
+/**
+ * Function called on request error
+ * @param err 
+ */
+function onRequestError(err) {
 	console.log(err);
 }
 
-let getAltitude = raw => {
+/**
+ * Function that transforms the request raw payload into the altitude
+ * @param raw is the text received after the URL call
+ */
+function getAltitude(raw) {
+	// Remove the first characters and transform the rest as JSON
 	let json = JSON.parse(raw.substring(4));
+	// Parse the json file to reach the altitude value
 	return parseInt(json[1][0][5][0][1][1][0]);
 }
--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-document.body.innerHTML += `<div id="am-altitude-container"><p></p></div>`;
+/**
+ * script.js
+ * created on 06/08/2018
+ * by Jeremie Primas
+ */
 
-const setAltitude = altitude => {
+
+/**
+ * Function that injects the altitude value in the DOM container element
+ * @param altitude formatted altitude to inject in the DOM 
+ */
+function setAltitude(altitude) {
 	document.querySelector("#am-altitude-container > p").innerHTML = `${altitude}m`;
-};
+}
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+/**
+ * Function called on message received by the extension
+ * @param {*} message sent by the extension
+ * @param {*} sender identifies the message sender
+ * @param {*} sendResponse callback to notify the extension
+ */
+function onMessage(message, sender, sendResponse) {
 	if(message.origin && message.value && message.origin === "AltiMaps") {
 		setAltitude(message.value);
+		sendResponse({
+			origin: message.origin,
+			value: "Success"
+		});
 	}
-});
+	else {
+		sendResponse({
+			origin: "AltiMaps",
+			value: "Error: Arguments missing"
+		});
+	}
+}
+
+/**
+ * Entry function
+ */
+function main() {
+	// Inject the extension element inside the DOM body
+	document.body.innerHTML += `<div id="am-altitude-container"><p></p></div>`;
+	// Create a listener that trigger a callback function on message received
+	chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+		onMessage()
+	});
+}
